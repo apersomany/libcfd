3fa645e1dabe358ae6473966bf62fcc592e1de82
Chris Pick <chris@chrispick.com>
2025-03-06 12:59:19 -0500

    Move `capnp`-generated files into `$OUT_DIR`
    Previously, the build.rs build script would generate files in
    "src/generated".  This modification to the source tree would case
    read-only builds to fail.
    
    Instead, generate files into the
    [`$OUT_DIR`](https://doc.rust-lang.org/cargo/reference/build-scripts.html#outputs-of-the-build-script)
    as [recommended](https://docs.rs/capnpc/0.20.1/capnpc/index.html).
    
    Fixes: https://github.com/apersomany/libcfd/issues/4

--- build.rs
+++ build.rs
@@ -3,7 +3,6 @@ use capnpc::CompilerCommand as CapnpcCompilerCommand;
 fn main() {
     CapnpcCompilerCommand::new()
         .import_path("src/generated")
-        .output_path("src/generated")
         .src_prefix("src/generated")
         .default_parent_module(Vec::from_iter([String::from("generated")]))
         .file("src/generated/quic_metadata_protocol.capnp")
